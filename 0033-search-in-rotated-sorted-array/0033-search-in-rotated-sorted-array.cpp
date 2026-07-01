5451e5ada51d5c12b1bb26d18837a5a873ce313b
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-01 22:10:13 +0530

    Time: 0 ms (100%), Space: 15.2 MB (67.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -9,7 +9,7 @@ public:
        } 
      return -1;
     }
-};*/
+};
 
 //optimal solution using binary search O(LOg N)
 class Solution {
@@ -24,7 +24,7 @@ public:
             else
                 high = mid - 1;
         }
-        return -1;
+         return -1;
     }
     int search(vector<int>& nums, int target) {
         int n = nums.size();
@@ -44,5 +44,36 @@ public:
             return binarySearch(nums, 0, pivot - 1, target);
         // Target in right half
         return binarySearch(nums, pivot, n - 1, target);
+    }
+};*/
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+                return mid;
+            // Left half sorted
+            if(nums[low] <= nums[mid]) {
+                if(target >= nums[low] &&
+                   target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // Right half sorted
+            else {
+                if(target > nums[mid] &&
+                   target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
     }
 };
