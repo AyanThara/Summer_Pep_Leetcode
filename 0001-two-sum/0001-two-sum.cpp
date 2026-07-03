2542052c7d8195f3fd3c07c957e8588bd0ce4238
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-03 22:08:06 +0530

    Time: 42 ms (25.4%), Space: 14.1 MB (68.54%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {//brute force using two loops 
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {//check using two loops whether the sum of two numbers is equal to the target if yes return the pair sum
+                    return {i, j}; 
+                }
+            }
+        }
+        return {}; 
+    }
+};
