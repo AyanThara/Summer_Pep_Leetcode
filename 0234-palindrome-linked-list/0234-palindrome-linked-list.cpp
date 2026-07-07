0e8c890e5d20a3d5a807ab1df23454de9080c439
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-07 11:04:57 +0530

    Time: 0 ms (100%), Space: 117.9 MB (82.06%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -7,7 +7,7 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+ 
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
@@ -24,6 +24,40 @@ public:
             }
             start++;
             end--;
+        }
+        return true;
+    }
+};
+*/
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {//reverse function
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {//check palindrome function
+        if (head == NULL || head->next == NULL)
+            return true;
+        ListNode* slow = head;//calculate mid 
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* p1 = reverse(slow);
+        ListNode* p2 = head;
+        while (p1) {//start a pointer from head as p1 and slow as p2 and compare till p2 is not null return false if not equal else return true
+            if (p1->val != p2->val)
+                return false;
+            p1 = p1->next;
+            p2 = p2->next;
         }
         return true;
     }
