65821dc4fabb9aa3199aaca59fb0039c8f13bd61
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-10 22:47:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (37.22%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,28 +1,13 @@
-/*class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }for (int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-    }
-};*/
-
-
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-            swap(matrix[i][j],matrix[j][i]);
+        int n=matrix.size();//calculate the size of matrix 
+        for(int i=0;i<n;i++){//start a loop from 0,0 till n i is row wise
+            for(int j=i;j<n;j++){//start a loop from i till n j is row wise 
+            swap(matrix[i][j],matrix[j][i]);//swap i,j with j,1
             }
-        }for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
+        }for(int i=0;i<n;i++){//start a loop till n 
+            reverse(matrix[i].begin(),matrix[i].end());//reverse the cols
         }
     }
 };
