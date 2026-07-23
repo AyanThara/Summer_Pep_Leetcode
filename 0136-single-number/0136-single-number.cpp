4c6ff138ae0a7f2ed5718de33b871acc5f2564b7
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-23 11:39:41 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.11%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,11 @@
-class Solution {//using simple xor operator
+class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;//take a answer variable and start with 0 
-        for (int x : nums)//run a for loop on whole nums
-            ans ^= x;//xor ans with every val at that index 
-        return ans;//return the ans
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            ans^=nums[i];
+        }
+     return ans;
     }
 };
-//whole array will be cancle each other and unique element will be left
-
