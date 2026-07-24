ce738d1d3dd123b84496e8ff5f5e06241b085587
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-24 11:44:53 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,34 +1,15 @@
-class Solution {//kadanes algo 
+class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n=nums.size();//take a variable and intialize with nums.size
-        int currsum=0;//take currsum as 0
-        int maxsum=INT_MIN;//and max sum as -infinite
-        for(int i=0;i<n;i++){//run the loop till n 
-            currsum+=nums[i];//update currsum with nums[i]
-            maxsum=max(currsum,maxsum);//check max of currsum and maxsum
-            if(currsum<0)//if currsum id less than 0 reintilze it with 0
-            currsum=0;
+        int sum=0;
+        int maxsum=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxsum=max(maxsum,sum);
+            if(sum<0)
+            sum=0;
         }
-    return maxsum;//return max sum
+    return maxsum;
     }
 };
-/*
-class Solution {//brute force approach 
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int maxsum = nums[0];
-        for(int i = 0; i < n; i++){
-            int currsum = 0;   
-            for(int j = i; j < n; j++){
-                currsum += nums[j];   // use j
-                maxsum = max(maxsum, currsum);
-            }
-        }
-        return maxsum;
-    }
-};*/
-
-
-
