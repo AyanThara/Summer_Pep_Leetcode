6ee829d984adda466ec19a4e7493a4ee3f48bd7e
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-30 23:09:12 +0530

    Time: 0 ms (100%), Space: 26 MB (94.22%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,11 +5,11 @@ public:
         int right = nums.size() - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (mid % 2 == 1) // Make mid even
+            if (mid % 2 == 1)// Make mid even
                 mid--;
-            if (nums[mid] == nums[mid + 1]) { // Single element is on the right
+            if (nums[mid] == nums[mid + 1]) {// Single element is on the right
                 left = mid + 2;
-            } else {  // Single element is on the left (including mid)
+            } else { // Single element is on the left (including mid)
                 right = mid;
             }
         }
