c25a177c16b3c18a666253373d59835a6f1ae027
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-30 23:08:51 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.58%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,38 +1,18 @@
-class Solution {//binary search
+class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        //corner case to check if only 1 element is present then return it
-        int start=0;
-        int end=n-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;//cal mid
-            if(mid==0 && nums[0]!=nums[1]) return nums[0];
-            //corner case for first element i.e if first element is not equal to second element then its only single element as there is no element before it to compare with
-            if(mid==n-1 && nums[n-1]!=nums[n-2]) return nums[n-1];
-            //corner case for last element that if last element is not equal to element before it then it is only the single element as there is no element after that to compare with
-            if(mid>0 && mid<n-1 &&
-               nums[mid]!=nums[mid-1] &&
-               nums[mid]!=nums[mid+1])
-                return nums[mid];
-            //compare with -1 and +1 element for mid if none match then return mid
-            if(mid%2==0){
-                //even in case the mid is even
-                if(nums[mid]==nums[mid+1]){//left case
-                    start=mid+1;
-                }else{//right case
-                    end=mid-1;
-                }
-            }else{
-                //odd
-                if(nums[mid]==nums[mid-1]){//right case
-                    start=mid+1;
-                }else{//left case 
-                    end=mid-1;
-                }
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) // Make mid even
+                mid--;
+            if (nums[mid] == nums[mid + 1]) { // Single element is on the right
+                left = mid + 2;
+            } else {  // Single element is on the left (including mid)
+                right = mid;
             }
         }
-     return -1;
+        return nums[left];
     }
 };
