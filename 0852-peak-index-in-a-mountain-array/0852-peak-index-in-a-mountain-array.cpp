5b744fd30503dac8fc74c9ed7d6ab07f54c470d9
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-08-03 11:30:31 +0530

    Time: 0 ms (100%), Space: 63.5 MB (79.85%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,6 +5,8 @@ public:
         int right = arr.size() - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1])
+            return mid;
             if (arr[mid] < arr[mid + 1]) {
                 left = mid + 1;
             } else {
