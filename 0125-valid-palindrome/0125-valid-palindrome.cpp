651b3cf96e3e1261254357fa913f299767cf4a8e
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-08-06 23:10:12 +0530

    Time: 0 ms (100%), Space: 10 MB (54.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0;
@@ -15,6 +15,31 @@ public:
             }
             left++;
             right--;
+        }return true;
+    }
+};*/
+class Solution {
+public:
+    bool isAlphaNum(char ch){
+        if ((ch >= 'a' && ch <= 'z') ||(ch >= 'A' && ch <= 'Z') ||
+            (ch >= '0' && ch <= '9')){
+            return true;
+        }return false;
+    }
+    bool isPalindrome(string s){
+        int st = 0;
+        int end = s.length() - 1;
+        while (st < end){
+            if (!isAlphaNum(s[st])){ // Skip invalid character from left
+                st++;
+                continue;   // Skip invalid character from right
+            }if (!isAlphaNum(s[end])){
+                end--;
+                continue;
+            }if (tolower(s[st]) != tolower(s[end])){ // Compare characters
+                return false;
+            }st++;// Move both pointers
+            end--;
         }return true;
     }
 };
