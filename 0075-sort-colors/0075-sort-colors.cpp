acb02106bbb5b855bc49efa5b74387365e5a0d34
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-08-11 23:02:47 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,28 @@
+/*class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());//brute force approach using in build sorting function o(nlogn)
+    }
+};*/
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0;
-        int mid = 0;
+        int low = 0, mid = 0;
         int high = nums.size() - 1;
-        while(mid <= high) {
-            if(nums[mid] == 0) {
+        while (mid <= high) {
+            if (nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
                 low++;
                 mid++;
-            }else if(nums[mid] == 1) {
+            }
+            else if (nums[mid] == 1) {
                 mid++;
             }
-            else {
+            else { 
                 swap(nums[mid], nums[high]);
                 high--;
             }
         }
     }
 };
+
