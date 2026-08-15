472ed8961c560556611998f7f0b5562ec24ec340
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-08-15 22:50:24 +0530

    Time: 0 ms (100%), Space: 27 MB (98.74%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -19,6 +19,12 @@ public:
 };*/class Solution {
 public:
     void reverseString(vector<char>& s) {
-      reverse(s.begin(),s.end());  
+     int start=0;
+     int end=s.size()-1;
+     while(start<end){
+        swap(s[start],s[end]);
+        start++;
+        end--;
+     } 
     }
 };
