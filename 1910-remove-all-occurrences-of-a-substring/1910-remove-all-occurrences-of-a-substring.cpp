9240ae05eed55243217566cf35ace239b23d92ff
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-08-28 14:38:11 +0530

    Time: 0 ms (100%), Space: 9.4 MB (41.01%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length()>0 && s.find(part)<s.length()){
-         s.erase(s.find(part),part.length());
-        }
-        return s;
+        while(s.find(part)!=string::npos){
+        int pos=s.find(part);
+        s.erase(pos,part.length());
+        }return s;
     }
 };
